0c20e08385e94f62bce1720ca95c9ce1658306c9
professorric <93748936+professorric@users.noreply.github.com>
2024-11-18 19:30:06 +0800

    Update Fizz Buzz.cpp

--- Fizz Buzz/Fizz Buzz.cpp
+++ Fizz Buzz/Fizz Buzz.cpp
@@ -2,23 +2,23 @@
 using namespace std;
 
 int main() {
-    // Loop from 1 to 15
+    
     for (int i = 1; i <= 15; ++i) {
-        // Check if the number is divisible by both 3 and 5
+     
         if (i % 3 == 0 && i % 5 == 0) {
-            cout << "FizzBuzz" << endl;  // For numbers divisible by both 3 and 5
+            cout << "FizzBuzz" << endl;  
         }
-        // Check if the number is divisible by 3 only
+     
         else if (i % 3 == 0) {
-            cout << "Fizz" << endl;  // For numbers divisible by 3
+            cout << "Fizz" << endl;  
         }
-        // Check if the number is divisible by 5 only
+   
         else if (i % 5 == 0) {
-            cout << "Buzz" << endl;  // For numbers divisible by 5
+            cout << "Buzz" << endl;  
         }
-        // Otherwise, print the number itself
+        
         else {
-            cout << i << endl;  // For numbers not divisible by 3 or 5
+            cout << i << endl;  
         }
     }
 
